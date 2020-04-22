e03ea8222851e50dd40e6898e3de72b3c406f2c2
Vladymyr Protsenko <grayfolk@gmail.com>
2020-04-23 02:21:10 +0300

    Update SQL for query by interests

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -7,6 +7,9 @@ use yii\web\Response;
 use yii\filters\ContentNegotiator;
 use yii\db\Expression;
 use app\models\News;
+use app\models\Site;
+use app\models\SiteInterest;
+use app\models\Interest;
 
 class ApiController extends Controller
 {
@@ -38,8 +41,26 @@ class ApiController extends Controller
     {
         $request = Yii::$app->request;
         
-        file_put_contents(__DIR__ . '/api.log', print_r($request->getHeaders(), true), FILE_APPEND);
-        file_put_contents(__DIR__ . '/api.log', print_r($request->getHeaders()->get('Interests'), true), FILE_APPEND);
-        return News::find()->orderBy(new Expression('rand()'))->one();
+        if (! Yii::$app->request->get('interests'))
+            return News::find()->orderBy(new Expression('rand()'))->one();
+            
+            // Request with interests id
+            // SQL query with move news w/o interests to end of results
+            // SELECT n.* FROM `news` n join site s on n.site_id = s.id left join site_interest si on s.id = si.site_id left join interest i on si.interest_id = i.id where i.id in(1, 2) or i.id is null order by case when i.id is null then 1 else 0 end, rand()
+        
+        return (new \yii\db\Query())->select([
+            News::tableName() . '.*'
+        ])
+        ->from(News::tableName())
+        ->leftJoin(Site::tableName(), Site::tableName() . '.[[id]] = ' . News::tableName() . '.[[site_id]]')
+        ->leftJoin(SiteInterest::tableName(), SiteInterest::tableName() . '.[[site_id]] = ' . Site::tableName() . '.[[id]]')
+        ->leftJoin(Interest::tableName(), Interest::tableName() . '.[[id]] = ' . SiteInterest::tableName() . '.[[interest_id]]')
+        ->where([
+            'in',
+            Interest::tableName() . '.[[id]]',
+            $request->get('interests')
+        ])->orWhere([
+            Interest::tableName() . '.[[id]]' => null
+        ])->orderBy(new Expression('case when ' . Interest::tableName() . '.[[id]] is null then 1 else 0 end, rand()'))->one();
     }
 }
